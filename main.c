087d75d395169ee07d25b4935c010b27116c7c00
edu-m <mledrd02e15c351o@studium.unict.it>
2023-07-30 19:23:11 +0200

    using input handler

--- main.c
+++ main.c
@@ -55,11 +55,12 @@ void parse_input(char *s, char *p,
 void op_gen(char *op, char *ops, int *n1, int *n2) {
   *op = ops[rand() % 4];
   *n1 = rand() % MAX_NUM + 1;
-  *n1 +=
-      (*op == '/' && *n1 % 2 != 0); // generates a "friendlier" set of numbers
-                                    // to compute when division occurs
-  if (*op == '/') // so that there will always an integer division with lowish
-                  // numbers in denominator
+  // generates a "friendlier" set of numbers
+  // to compute when division occurs so that
+  // there will always an integer division
+  // with lowish numbers in the denominator
+  *n1 += (*op == '/' && *n1 % 2 != 0);
+  if (*op == '/')
     do {
       *n2 = ((rand() % *n1 / 2)) + 2;
     } while (*n1 % *n2 != 0);
@@ -81,8 +82,21 @@ void parse_args(int argc, char **argv, int *q_amt) {
   }
 }
 
-int compare(const long *input, const long *result) {
-  return (*input == *result);
+int compare(const int *input, const int *result) { return (*input == *result); }
+
+void print_score(const int *correct_answers) {
+  print_c(BLUE, "Correct Answers: %d\n", *correct_answers);
+}
+
+void handle_input_correct(int *correct_answers) {
+  ++(*correct_answers);
+  print_c(GREEN, "Correct!\n");
+  print_score(correct_answers);
+}
+
+void handle_input_wrong(const int *correct_answers, int *result) {
+  print_c(RED, "Result is %d. Try again!\n", *result);
+  print_score(correct_answers);
 }
 
 int main(int argc, char **argv) {
@@ -90,6 +104,8 @@ int main(int argc, char **argv) {
   banner();
   char ops[4] = {'*', '+', '-', '/'};
   int (*fx[4])(int, int) = {add, mul, sub, divide};
+  void (*handle_outcomes[2])(int *, int *) = {handle_input_wrong,
+                                              handle_input_correct};
   int n1, n2, result, q_amt;
   char op, *p, s[MAX_INPUT_SIZE];
   long input;
@@ -101,11 +117,10 @@ int main(int argc, char **argv) {
     op_gen(&op, ops, &n1, &n2);
     printf("%d %c %d\n> ", n1, op, n2);
     parse_input(s, p, &input);
+    printf("%ld\n", input);
     result = fx[get_value(op)](n1, n2);
 
-    ((correct_answers += compare(&input, (long *)&result))
-         ? print_c(GREEN, "Correct!\n")
-         : print_c(RED, "Result is %d. Try again!\n", result));
+    handle_outcomes[compare((int *)&input, &result)](&correct_answers, &result);
   }
   printf("\a\aYou scored %d out of %d! (%1.f\%)\n", correct_answers, q_amt_copy,
          ((correct_answers * 100) / (float)(q_amt_copy)));
