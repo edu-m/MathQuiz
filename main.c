f67f6b787b198505aca3847083087e6e7b74fdb6
edu-m <mledrd02e15c351o@studium.unict.it>
2023-07-31 01:01:19 +0200

    bug fix, op enum

--- main.c
+++ main.c
@@ -16,7 +16,8 @@ int mul(int x, int y) { return x * y; }
 int sub(int x, int y) { return x - y; }
 int divide(int x, int y) { return x / y; }
 
-static void die(const char *fmt, ...) {
+static void die(const char *fmt, ...)
+{
   va_list argp;
   va_start(argp, fmt);
   vfprintf(stderr, fmt, argp);
@@ -27,32 +28,45 @@ static void die(const char *fmt, ...) {
 
 void banner() { printf("\t\t\tMath Quiz\n\n\t\t  By Eduardo Meli 2023\n\n"); }
 
+typedef enum
+{
+  OP_ADD,
+  OP_MUL,
+  OP_SUB,
+  OP_DIV
+} op_type;
+
 int get_value(const char op) // hacky bit, maps to function pointer array which
                              // function to compute
 {
   if (op == '+')
-    return 0;
+    return OP_ADD;
   if (op == '*')
-    return 1;
+    return OP_MUL;
   if (op == '-')
-    return 2;
+    return OP_SUB;
   if (op == '/')
-    return 3;
+    return OP_DIV;
   return -1;
 }
 
 void parse_input(char *s, char *p,
-                 long *input) { // parse the input to ignore all non-integers
-  while (fgets(s, sizeof(s), stdin)) {
+                 long *input)
+{ // parse the input to ignore all non-integers
+  while (fgets(s, sizeof(s), stdin))
+  {
     *input = strtol(s, &p, 10);
-    if (p == s || *p != '\n') {
+    if (p == s || *p != '\n')
+    {
       printf("Please enter an integer: ");
-    } else
+    }
+    else
       break;
   }
 }
 
-void op_gen(char *op, char *ops, int *n1, int *n2) {
+void op_gen(char *op, char *ops, int *n1, int *n2)
+{
   *op = ops[rand() % 4];
   *n1 = rand() % MAX_NUM + 1;
   // generates a "friendlier" set of numbers
@@ -61,18 +75,21 @@ void op_gen(char *op, char *ops, int *n1, int *n2) {
   // with lowish numbers in the denominator
   *n1 += (*op == '/' && *n1 % 2 != 0);
   if (*op == '/')
-    do {
+    do
+    {
       *n2 = ((rand() % *n1 / 2)) + 2;
     } while (*n1 % *n2 != 0);
   else
     *n2 = rand() % MAX_NUM;
 }
 
-void parse_args(int argc, char **argv, int *q_amt) {
+void parse_args(int argc, char **argv, int *q_amt)
+{
   const char *arg;
   int user_amt;
   *q_amt = 10;
-  for (int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++)
+  {
     if (*argv[i] != '-')
       continue;
     if (*(argv[i] + 1) == 'c' && (user_amt = atoi(argv[i] + 2)) > 0)
@@ -84,28 +101,32 @@ void parse_args(int argc, char **argv, int *q_amt) {
 
 int compare(const int *input, const int *result) { return (*input == *result); }
 
-void print_score(const int *correct_answers) {
+void print_score(const int *correct_answers)
+{
   print_c(BLUE, "Correct Answers: %d\n", *correct_answers);
 }
 
-void handle_input_correct(int *correct_answers) {
+void handle_input_correct(int *correct_answers, const int *result)
+{
   ++(*correct_answers);
   print_c(GREEN, "Correct!\n");
   print_score(correct_answers);
 }
 
-void handle_input_wrong(const int *correct_answers, int *result) {
+void handle_input_wrong(int *correct_answers, const int *result)
+{
   print_c(RED, "Result is %d. Try again!\n", *result);
   print_score(correct_answers);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   srand(time(NULL));
   banner();
   char ops[4] = {'*', '+', '-', '/'};
   int (*fx[4])(int, int) = {add, mul, sub, divide};
-  void (*handle_outcomes[2])(int *, int *) = {handle_input_wrong,
-                                              handle_input_correct};
+  void (*handle_outcomes[2])(int *, const int *) = {handle_input_wrong,
+                                                    handle_input_correct};
   int n1, n2, result, q_amt;
   char op, *p, s[MAX_INPUT_SIZE];
   long input;
@@ -113,16 +134,16 @@ int main(int argc, char **argv) {
   int correct_answers = 0;
   parse_args(argc, argv, &q_amt);
   int q_amt_copy = q_amt;
-  MAIN_LOOP {
+  MAIN_LOOP
+  {
     op_gen(&op, ops, &n1, &n2);
     printf("%d %c %d\n> ", n1, op, n2);
     parse_input(s, p, &input);
-    printf("%ld\n", input);
     result = fx[get_value(op)](n1, n2);
 
     handle_outcomes[compare((int *)&input, &result)](&correct_answers, &result);
   }
-  printf("\a\aYou scored %d out of %d! (%1.f\%)\n", correct_answers, q_amt_copy,
+  printf("\a\aYou scored %d out of %d! (%1.f%%)\n", correct_answers, q_amt_copy,
          ((correct_answers * 100) / (float)(q_amt_copy)));
   return 0;
 }
